6e8e6a7653bbbd7b59cbbbdd2d171b94029d7023
elixir-dhruvil <dhruvil.cloud12@gmail.com>
2025-05-09 12:39:18 +0530

    Update the header component with proper style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,9 +49,6 @@ function Header() {
         <nav className="flex items-center justify-between h-16">
           
           <NavLink to="/" className="flex items-center space-x-2">
-            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd" />
-            </svg> */}
             <span className="text-2xl font-bold text-blue-600">NewsHub</span>
           </NavLink>
 
@@ -80,19 +77,6 @@ function Header() {
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             className="lg:hidden p-2 rounded-md text-gray-600 hover:bg-gray-100 focus:outline-none"
           >
-            {/* <svg
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {isMenuOpen ? (
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-              ) : (
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              )}
-            </svg> */}
-            {/* {isMenuOpen ? '✕' : '☰'} */}
             <FontAwesomeIcon 
               icon={isMenuOpen ? faXmark : faBars} 
               className="h-6 w-6"
